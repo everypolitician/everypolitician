b24b5d3bf4a062569f5ea9ea2b96f7616e0a6f3c
Chris Mytton <chrismytton@gmail.com>
2015-02-20 16:07:10 +0000

    Start popit import after upload
    Eventually the import won't happen immediately after the upload. Instead
    there will be a button that allows the user to optionally upload to
    popit, however this is good enough for a POC.

---
+++ assets/scripts/main.js
@@ -0,0 +1,39 @@
+(function($) {
+  function popitImport(instanceSlug, popoloJson) {
+    var popitInstanceUrlTemplate = $('meta[name="popit-instance-url-template"]').attr('content');
+    var url = popitInstanceUrlTemplate.replace('{instance}', instanceSlug);
+    return $.ajax({
+      type: 'POST',
+      url: url,
+      data: JSON.stringify(popoloJson),
+      processData: false,
+      dataType: 'json',
+      contentType: 'application/json',
+      // Make sure the Cookie header gets sent
+      xhrFields: {
+        withCredentials: true
+      }
+    });
+  }
+
+  Dropzone.options.myAwesomeDropzone = {
+    dictDefaultMessage: "Drag and drop your file here, or click to upload",
+    uploadMultiple: false,
+    createImageThumbnails: false,
+    acceptedFiles: 'text/csv',
+    previewsContainer: '.preview-area',
+    paramName: 'csv',
+    init: function() {
+      this.on('success', function(file, json) {
+        // Post json over to popit importer
+        popitImport('import-test1', json)
+        .done(function(response) {
+          console.log(response);
+        })
+        .fail(function(xhr, status, errorThrown) {
+          console.error("Couldn't start import:", errorThrown);
+        });
+      });
+    }
+  };
+})(window.jQuery);
